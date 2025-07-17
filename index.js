a316e91244ac9db39597d4fb08c05245a63f6e12
saroj14-png <sarojani.powar@gmail.com>
2025-07-17 15:27:20 -0700

    added Javascipt file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-template_e6i852b;
-service_t94vdqo;
-f4nj8ADMO_NsaWaEI;
+
+function contact(event){
+    event.preventDefault();
+    emailjs
+    .sendform(
+        'service_t94vdqo', 
+        'template_e6i852b', 
+        event.target, 
+        'user_f4nj8ADMO_NsaWaEI'
+    ).then(() => {
+            console.log('SUCCESS!')})
+        }
