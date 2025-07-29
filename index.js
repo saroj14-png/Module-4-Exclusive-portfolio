8802ff398a659ba7a25c8e700b9c5a6b54a09774
saroj14-png <sarojani.powar@gmail.com>
2025-07-29 14:46:36 -0700

    contact function

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
-function contact(event){
-    event.preventDefault();
-    emailjs
-    .sendform(
-        'service_t94vdqo', 
-        'template_e6i852b', 
-        event.target, 
-        'user_f4nj8ADMO_NsaWaEI'
-    ).then(() => {
-            console.log('SUCCESS!')})
+function contact(){
+    //event.preventDefault();
+    //emailjs
+    //  .sendForm(
+    //    "service_t94vdqo",
+    //    "template_e6i852b",
+    //    event.target,
+     //   "f4nj8ADMO_NsaWaEI"
+     // )
+     // .then(() => {
+     //   console.log("SUCCESS!");
+     // });
+     const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading .classList += "  modal__overlay--visible"
+     setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += "  modal__overlay--visible";
+        console.log("Set a time out from mail!")}, 500);
         }
