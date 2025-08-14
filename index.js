f89471ad23172eaf440960e06c72a26948783fbb
saroj14-png <sarojani.powar@gmail.com>
2025-08-14 11:00:42 -0700

    movebackground js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor=1/20;
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 === 0;
+    const boolInt = isOdd ? 1 : -1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -9,6 +20,8 @@ function toggleContrast() {
     document.body.classList.remove("dark-theme");
   }
 }
+
+
 
 function contact(event) {
   event.preventDefault();
@@ -37,8 +50,9 @@ function contact(event) {
 
 function toggleModal() {
   if (isModalOpen) {
-    return document.body.classList.remove("modal--open");
     isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+    
   }
   isModalOpen = true;
   //toggle modal
